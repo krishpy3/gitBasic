fab21492ed08f2e0f632d8b5d5194c4f3785a81b
Navaneethakrishnan <krish.nava007@gmail.com>
2022-09-29 08:26:19 +0550

    Git initialized with GitPython

---
+++ README.md
@@ -0,0 +1,19 @@
+## Things we are going to discuss
+1. initialize or clone
+2. add - make the file trackable
+3. seeing the changes - status => untracked, modified, staged
+3. remove - untrack the file => .gitignore, git rm --cached file_name 
+4. commit - save the changes
+5. log - see the history
+6. add remote - connect to the remote repository
+7. push - send the changes to the remote repository
+8. pull - get the changes from the remote repository
+9. fetch - get the changes from the remote repository
+10. diff - see the changes  
+11. branch - create a new branch, switch to a branch, delete a branch (from local and remote)
+12. merge - merge the changes from one branch to another
+13. rebase - rebase the changes from one branch to another
+14. stash - save the changes temporarily
+15. reset - reset the changes -> hard, soft, mixed
+16. tag - create a tag, delete a tag
+
---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+database = [{"mid": 1, "year": 2013, "title": "Rush"},
+            {"mid": 2, "year": 2014, "title": "Thor: The Dark World"}]
+
+last_mid = 2
+
+ 
+
+@app.route('/movie')
+def list_movie():
+    dd = []
+    if request.args:
+        for m in database:
+            if m['year'] == int(request.args.get('year')):
+                dd.append(m)
+    else:
+        dd = database
+    return jsonify(dd)
+
+
+@app.route('/movie/<mid>')
+def get_movie(mid):
+    for m in database:
+        if m['mid'] == int(mid):
+            return jsonify(m)
+    return jsonify({'error': f'There is no movie with id: {mid}'})
+
+
+@app.route('/movie', methods=['POST'])
+def new_movie():
+    global last_mid
+    user_data = request.form
+    last_mid += 1
+    mov = {
+        "mid": last_mid,
+        "year": int(user_data.get('year', 1950)),
+        "title": user_data.get('title', "No Title")
+    }
+    database.append(mov)
+    return jsonify(mov)
+
+
+@app.route('/movie/<mid>', methods=['PUT'])
+def update_movie(mid):
+    index = 0
+    for m in database:
+        if m['mid'] == int(mid):
+            year = m['year']
+            title = m['title']
+            break
+        index += 1
+
+    mov = {
+        "mid": mid,
+        "year": int(request.form.get('year', year)),
+        "title": request.form.get('title', title)
+    }
+    database.pop(index)
+    database.insert(index, mov)
+    return jsonify(mov)
+
+@app.route('/movie/<mid>', methods=['DELETE'])
+def delete_movie(mid):
+    index = 0
+    for m in database:
+        if m['mid'] == int(mid):
+            database.pop(index)
+            break
+        index += 1
+    return jsonify({'msg': f'Movie with id {mid} deleted successfully'})
+
+
+
+app.run(debug=True)
